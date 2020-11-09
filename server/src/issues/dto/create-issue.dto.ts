a636ccc5258d7a8773adcdac897aa5594924efdc
tunatone0111 <tunatone.0111@gmail.com>
2020-11-10 01:22:38 +0900

    [feat] add validation pipe

--- server/src/issues/dto/create-issue.dto.ts
+++ server/src/issues/dto/create-issue.dto.ts
@@ -1,7 +1,14 @@
+import { IsNumber, IsString, Length } from 'class-validator';
 import { Prec } from 'src/precs/schemas/prec.schema';
 
 export class CreateIssueDto {
-  text: string;
-  embedding: number[];
-  prec: Prec;
+  @IsString()
+  readonly text: string;
+
+  @IsNumber()
+  @Length(10)
+  readonly embedding: number[];
+
+  @IsString()
+  readonly prec: Prec;
 }
