b83743a48df3d7bf56aaa9904786d4ca97625026
tunatone0111 <tunatone.0111@gmail.com>
2020-11-15 18:24:16 +0900

    [feat] crawler fix

--- webCrawler/crawler.py
+++ webCrawler/crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import NavigableString
 from bs4 import BeautifulSoup
 import json
-from tqdm import tqdm
+from tqdm import tqdm, trange
 import re
 
 
@@ -15,62 +15,67 @@ def parse_statements(raw):
     return ss, n if n == 1 else n-1
 
 
-N = 10
+N = 10000
+for page in [9]:
+    source = requests.get(
+        f"http://www.law.go.kr/precScListR.do?q=*&section=bdyText&outmax={N}&pg={page}&fsort=21,10,30&precSeq=0&dtlYn=N").text
 
-source = requests.get(
-    f"http://www.law.go.kr/precScListR.do?q=*&section=bdyText&outmax={N}&pg=1&fsort=21,10,30&precSeq=0&dtlYn=N").text
+    soup = BeautifulSoup(source, "lxml")
 
-soup = BeautifulSoup(source, "lxml")
+    anchors = soup.select("td.tl > a")
 
-anchors = soup.select("td.tl > a")
+    ids = [anchor["onclick"].split("'")[1] for anchor in anchors]
 
-ids = [anchor["onclick"].split("'")[1] for anchor in anchors]
+    precs = list()
+    for id in tqdm(ids):
+        text = requests.get(
+            f"https://www.law.go.kr/precInfoR.do?precSeq={id}&vSct=*").text
+        soup = BeautifulSoup(text, "lxml")
 
-precs = list()
-for id in tqdm(ids):
-    text = requests.get(
-        f"https://www.law.go.kr/precInfoR.do?precSeq={id}&vSct=*").text
-    soup = BeautifulSoup(text, "lxml")
+        prec = dict()
+        try:
+            subtitle = soup.select_one(".subtit1").text.strip()
+            prec['date'] = re.findall(
+                '[0-9]{4}\. [0-9]{1,2}\. [0-9]{1,2}\.', subtitle)[0]
+            prec['caseNum'] = re.findall('[0-9]{4}.[0-9]{4,6}', subtitle)[0]
 
-    prec = dict()
-    try:
-        sa = soup.select_one(".subtit1")
-        prec['caseNum'] = re.findall('[0-9]{4}.[0-9]{4,6}', sa.text.strip())[0]
-
-        sa = soup.select_one("#sa")
-        p = sa.find_next("p")
-        prec['issues'], prec['order'] = parse_statements(p.text)
-
-        sa = soup.select_one("#yo")
-        p = sa.find_next("p")
-        prec['yo'], _ = parse_statements(p.text)
-
-        sa = soup.select_one("#conLsJo")
-        if sa != None:
+            sa = soup.select_one("#sa")
             p = sa.find_next("p")
-            prec['refClauses'] = [[j.strip() for j in jo.split(',')]
-                                  for jo in parse_statements(p.text)[0]]
+            prec['issues'], prec['order'] = parse_statements(p.text)
 
-        sa = soup.select_one("#conPrec")
-        if sa != None:
+            sa = soup.select_one("#yo")
             p = sa.find_next("p")
-            prec['refPrecs'] = [re.findall(
-                '[0-9]{4}.[0-9]{4,6}', prec) for prec in parse_statements(p.text)[0]]
+            prec['yo'], _ = parse_statements(p.text)
 
-        sa = soup.select_one("#jun")
-        junmun = ""
-        for sibling in sa.next_siblings:
-            if isinstance(sibling, NavigableString):
-                continue
-            junmun += sibling.text
+            sa = soup.select_one("#conLsJo")
+            if sa != None:
+                p = sa.find_next("p")
+                prec['refClauses'] = [[j.strip() for j in jo.split(',')]
+                                      for jo in parse_statements(p.text)[0]]
 
-        prec['wholePrec'] = re.sub('\\n.*', '', junmun)
-        prec['judge'] = re.findall('\\n.*', junmun)[0].strip()
+            sa = soup.select_one("#conPrec")
+            if sa != None:
+                p = sa.find_next("p")
+                prec['refPrecs'] = [[] for _ in range(prec['order'])]
+                for i in p.text.split('/'):
+                    tmp = re.findall('[0-9]{4}.[0-9]{4,6}', i.strip())
+                    for j in re.findall('\[[0-9]\]', i.strip()):
+                        prec['refPrecs'][int(j[1])-1].extend(tmp)
 
-        precs.append(prec)
+            sa = soup.select_one("#jun")
+            junmun = ""
+            for sibling in sa.next_siblings:
+                if isinstance(sibling, NavigableString):
+                    continue
+                junmun += sibling.text
 
-    except:
-        continue
+            prec['wholePrec'] = re.sub('\\n.*', '', junmun)
+            prec['judge'] = re.findall('\\n.*', junmun)[0].strip()
 
-with open('precTot.json', 'w', encoding='UTF-8') as f:
-    f.write(json.dumps(precs, indent=2, ensure_ascii=False))
+            precs.append(prec)
+
+        except:
+            continue
+
+    with open(f'precs/precs_{page}.json', 'w', encoding='UTF-8') as f:
+        f.write(json.dumps(precs, indent=2, ensure_ascii=False))
