74433ee3f6950d327dd1a41c356a3c0877852811
tunatone0111 <tunatone0111.gmail.com>
2020-11-23 15:52:37 +0000

    [refactor] comparator

--- bert-server/app/benchmark.py
+++ bert-server/app/benchmark.py
@@ -3,40 +3,31 @@ from tqdm.contrib.concurrent import process_map
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-from tfidf import TfidfComparator
+from comparator import BertComparator, TfidfComparator
 
 db = DataBase()
 
-MAX = 10000
+MAX = 1000
 
-# comp_vectors = []
-# comp_precs = []
-# for x in tqdm(db.issues.find(projection={"vector": True, 'prec': True}), desc='loading issues'):
-#     comp_vectors.append(np.array(x['vector']) / np.linalg.norm(x['vector']))
-#     comp_precs.append(x['prec'])
-# comp_vectors = np.array(comp_vectors)
+# bert_comparator = BertComparator()
+tfidf_comparator = TfidfComparator()
 
 
-# def get_rank(test_issue):
+# def get_rank_bert(test_issue):
 #     test_vector = np.array(test_issue['vector']).reshape(-1, 1) / \
 #         np.linalg.norm(test_issue['vector'])
 
-#     sim_tab = np.matmul(comp_vectors, test_vector) ** 2
-#     sim_tab_sorted_idxs = sim_tab.reshape(-1).argsort()[::-1]
+#     sim_precs, idxs = bert_comparator.get_sim_precs_with_idxs(test_vector)
 
 #     ranks = []
 #     for ref_prec in test_issue['refPrecs']:
 #         try:
-#             idx = comp_precs.index(ref_prec)
-#             rank = np.where(sim_tab_sorted_idxs == idx)[0][0]
+#             rank = np.where(sim_precs == ref_prec)[0][0]
 #             ranks.append(rank)
 #         except:
 #             pass
 
 #     return (np.min(ranks) if any(ranks) else -1)
-
-
-tfidf_comparator = TfidfComparator()
 
 
 def get_rank_tfidf(test_issue):
--- bert-server/app/comparator.py
+++ bert-server/app/comparator.py
@@ -1,24 +1,85 @@
-from app.db import DataBase
+from konlpy.tag import Mecab
+from db import DataBase
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+from tqdm import tqdm
 
 
 class Comparator():
     def __init__(self):
-        print('initialize comparator...')
-        db = DataBase()
+        self.db = DataBase()
+        self.precs = []
+
+
+class BertComparator(Comparator):
+    def __init__(self):
+        print('initialize BertComparator...')
+        super().__init__()
         comp_vectors = []
-        comp_precs = []
-        for x in db.issues.find(projection={"vector": True, 'prec': True}):
+        for x in tqdm(self.db.issues.find(projection={"vector": True, 'prec': True})):
             comp_vectors.append(
                 np.array(x['vector']) / np.linalg.norm(x['vector']))
-            comp_precs.append(x['prec'])
+            self.precs.append(x['prec'])
         self.comp_vectors = np.array(comp_vectors)
-        self.comp_precs = np.array(comp_precs)
+        self.precs = np.array(self.precs)
 
-    def get_sim_precs(self, vec):
+    def get_sim_precs_with_idxs(self, vec):
         vec = np.array(vec.reshape(-1, 1) / np.linalg.norm(vec))
         sim_tab = np.matmul(self.comp_vectors, vec) ** 2
-        sim_tab_sorted_idxs = sim_tab.reshape(-1).argsort()[::-1]
+        idxs = sim_tab.reshape(-1).argsort()[::-1]
 
-        print(sim_tab_sorted_idxs.shape, sim_tab_sorted_idxs.dtype)
-        return self.comp_precs[sim_tab_sorted_idxs]
+        idx = np.unique(self.precs[idxs], return_index=True)[1]
+        sim_precs = self.precs[idxs][sorted(idx)]
+
+        return sim_precs, idxs
+
+
+class TfidfComparator(Comparator):
+    def __init__(self):
+        print('initializing TfidfComparator...')
+        super().__init__()
+        self.mecab = Mecab()
+        issues_noun = []
+        for x in tqdm(self.db.issues.find(projection={"text": True, 'prec': True})):
+            issues_noun.append(' '.join(self.mecab.nouns(x['text'])))
+            self.precs.append(x['prec'])
+        self.precs = np.array(self.precs)
+
+        print('    fitting...')
+        self.tfidf_vectorizer = TfidfVectorizer(
+            min_df=1, max_features=10000, dtype=np.float32).fit(issues_noun)
+        self.tfidf_vectors = self.tfidf_vectorizer.transform(
+            issues_noun).toarray()
+
+    def get_sim_precs_with_idxs(self, target_string):
+        target_nouns = ' '.join(self.mecab.nouns(target_string))
+        target_vector = self.tfidf_vectorizer.transform(
+            [target_nouns]).toarray()
+        sim_mat = np.matmul(self.tfidf_vectors,
+                            np.transpose(target_vector)).reshape(-1)
+        idxs = sim_mat.argsort()[::-1]
+
+        idx = np.unique(self.precs[idxs], return_index=True)[1]
+        sim_precs = self.precs[idxs][sorted(idx)]
+
+        return sim_precs, idxs
+
+
+if __name__ == '__main__':
+    """ Example for TF-IDF comparator """
+    db = DataBase()
+    tfidf_comparator = TfidfComparator()
+
+    pipeline = [{'$project': {'text': True}}, {'$sample': {'size': 10}}]
+
+    targets = []
+    for x in db.issues.aggregate(pipeline):
+        targets.append(x['text'])
+    targets = np.array(targets)
+
+    for target in targets:
+        print('\n\ncalculating...')
+        print(f'input: {target}')
+        sim_precs, _ = tfidf_comparator.get_sim_precs_with_idxs(target)
+        for i in range(10):
+            print(f'#{i+1}: {sim_precs[i]}')
