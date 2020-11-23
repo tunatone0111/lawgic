4620484e3d40d675f535c347ff144854480da5ca
tunatone0111 <tunatone0111.gmail.com>
2020-11-23 14:58:19 +0000

    [feat] tfidf

--- bert-server/app/benchmark.py
+++ bert-server/app/benchmark.py
@@ -3,31 +3,50 @@ from tqdm.contrib.concurrent import process_map
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+from tfidf import TfidfComparator
 
 db = DataBase()
 
 MAX = 10000
 
-comp_vectors = []
-comp_precs = []
-for x in tqdm(db.issues.find(projection={"vector": True, 'prec': True}), desc='loading issues'):
-    comp_vectors.append(np.array(x['vector']) / np.linalg.norm(x['vector']))
-    comp_precs.append(x['prec'])
-comp_vectors = np.array(comp_vectors)
+# comp_vectors = []
+# comp_precs = []
+# for x in tqdm(db.issues.find(projection={"vector": True, 'prec': True}), desc='loading issues'):
+#     comp_vectors.append(np.array(x['vector']) / np.linalg.norm(x['vector']))
+#     comp_precs.append(x['prec'])
+# comp_vectors = np.array(comp_vectors)
 
 
-def get_rank(test_issue):
-    test_vector = np.array(test_issue['vector']).reshape(-1, 1) / \
-        np.linalg.norm(test_issue['vector'])
+# def get_rank(test_issue):
+#     test_vector = np.array(test_issue['vector']).reshape(-1, 1) / \
+#         np.linalg.norm(test_issue['vector'])
 
-    sim_tab = np.matmul(comp_vectors, test_vector) ** 2
-    sim_tab_sorted_idxs = sim_tab.reshape(-1).argsort()[::-1]
+#     sim_tab = np.matmul(comp_vectors, test_vector) ** 2
+#     sim_tab_sorted_idxs = sim_tab.reshape(-1).argsort()[::-1]
+
+#     ranks = []
+#     for ref_prec in test_issue['refPrecs']:
+#         try:
+#             idx = comp_precs.index(ref_prec)
+#             rank = np.where(sim_tab_sorted_idxs == idx)[0][0]
+#             ranks.append(rank)
+#         except:
+#             pass
+
+#     return (np.min(ranks) if any(ranks) else -1)
+
+
+tfidf_comparator = TfidfComparator()
+
+
+def get_rank_tfidf(test_issue):
+    sim_precs, idxs = tfidf_comparator.get_sim_precs_with_idxs(
+        test_issue['text'])
 
     ranks = []
     for ref_prec in test_issue['refPrecs']:
         try:
-            idx = comp_precs.index(ref_prec)
-            rank = np.where(sim_tab_sorted_idxs == idx)[0][0]
+            rank = np.where(sim_precs == ref_prec)[0][0]
             ranks.append(rank)
         except:
             pass
@@ -37,11 +56,11 @@ def get_rank(test_issue):
 
 pipeline = [
     {'$match': {'refPrecs': {'$ne': None}}},
-    {'$project': {'vector': 1, 'refPrecs': 1}},
+    {'$project': {'text': 1, 'vector': 1, 'refPrecs': 1}},
     {'$sample': {'size': MAX}}
 ]
 
-result = process_map(get_rank, list(
+result = process_map(get_rank_tfidf, list(
     db.issues.aggregate(pipeline)), max_workers=8, chunksize=1)
 
 plt.figure(figsize=(16, 12))
