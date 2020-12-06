d24f3f2812cc57cc28a9982f6d6460dc076936e0
tunatone0111 <tunatone0111.gmail.com>
2020-12-06 06:29:20 +0000

    [fix] autoComplete component

--- web/src/components/SearchBox.jsx
+++ web/src/components/SearchBox.jsx
@@ -6,7 +6,7 @@ import "./SearchBox.css";
 export default function SearchBox() {
 	const [autoComplete, setAutoComplete] = useState([]);
 	const [spaceCursor, setSpaceCursor] = useState(0);
-	const [autoCursor, setAutoCursor] = useState(-1);
+	const [autoCursor, setAutoCursor] = useState(0);
 	const [textAreaContent, setTextAreaContent] = useState("");
 
 	function clearAutoComplete() {
@@ -54,7 +54,7 @@ export default function SearchBox() {
 	}
 
 	useEffect(() => {
-		setAutoCursor(-1);
+		setAutoCursor(0);
 	}, [autoComplete]);
 
 	function handleClick(e) {
@@ -72,7 +72,6 @@ export default function SearchBox() {
 					as="textarea"
 					id="text-input-box"
 					placeholder="사건을 입력하세요"
-					// dangerouslySetInnerHTML={{ __html: textAreaContent }}
 					value={textAreaContent}
 					onChange={handleOnChange}
 					onBlur={clearAutoComplete}
@@ -85,26 +84,54 @@ export default function SearchBox() {
 					document.querySelector("#text-input-box.form-control") ? (
 					<ListGroup
 						id="auto-list"
-						style={{ position: "absolute", top: "100%" }}
+						style={{
+							position: "absolute",
+							marginLeft: `${
+								0.75 +
+								(textAreaContent.split(" ").length - 1) * 0.1 +
+								(textAreaContent.slice(0, spaceCursor + 1).length -
+									(textAreaContent.split(" ").length - 1))
+							}rem`,
+							marginTop: `${2 * 1 + 0.375}rem`,
+							width: "auto",
+							opacity: 50,
+							zIndex: 4000
+						}}
 					>
 						{autoComplete.map((w, idx) => (
 							<ListGroup.Item
 								key={idx}
-								className={`auto-${idx} ${
+								className={`d-flex justify-content-between align-items-center p-1 auto-${idx} ${
 									autoCursor === idx ? "selected" : null
 								}`}
-								dangerouslySetInnerHTML={{
-									__html: w.replace(
-										textAreaContent.split(" ").splice(-1)[0],
-										`<span class="matched">${
-											textAreaContent.split(" ").splice(-1)[0]
-										}</span>`
-									)
+								style={{
+									width: "100%"
 								}}
 								onClick={handleClick}
 								value={w}
 								action
-							/>
+							>
+								<span
+									dangerouslySetInnerHTML={{
+										__html: w.replace(
+											textAreaContent.split(" ").splice(-1)[0],
+											`<span class="matched">${
+												textAreaContent.split(" ").splice(-1)[0]
+											}</span>`
+										)
+									}}
+								/>
+								<svg
+									height="1rem"
+									width="1rem"
+									className="ml-3"
+									enableBackground="new 0 0 280.823 280.823"
+									viewBox="0 0 280.823 280.823"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path d="m250.734 40.137-90.265-.02v20.059h90.265c5.534 0 10.029 4.515 10.029 10.049v80.216c0 5.534-4.496 10.029-10.029 10.029h-212.34l45.877-45.877-14.182-14.182-70.089 70.088 70.206 70.206 14.182-14.182-45.994-45.994h212.341c16.592 0 30.088-13.497 30.088-30.088v-80.216c0-16.592-13.497-30.088-30.089-30.088z" />
+								</svg>
+							</ListGroup.Item>
 						))}
 					</ListGroup>
 				) : null}
