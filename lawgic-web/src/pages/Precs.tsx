eceb27d9d7a0078a27a672d92ac1026564fcf8d8
Junha Park <tunatone.0111@gmail.com>
2021-04-05 18:46:59 +0000

    add prec detail view

--- lawgic-web/src/pages/Precs.tsx
+++ lawgic-web/src/pages/Precs.tsx
@@ -1,9 +1,30 @@
+import { Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import { useParams } from "react-router-dom";
+import BackButton from "../components/common/BackButton";
+const useStyles = makeStyles((theme) => ({
+	iframe: {
+		width: "100%",
+		height: "90vh",
+	},
+}));
 
 function Precs() {
+	const classes = useStyles();
 	const { id } = useParams<{ id: string }>();
-	return <div>prec id: {id}</div>;
+	return (
+		<Grid container>
+			<Grid item xs={1}>
+				<BackButton />
+			</Grid>
+			<Grid item xs={10}>
+				<iframe
+					className={classes.iframe}
+					src={`https://www.law.go.kr/LSW/precInfoP.do?precSeq=${id}&mode=0#AJAX`}
+				></iframe>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default Precs;
