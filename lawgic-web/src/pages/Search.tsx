c7a27a731353e3cf7c61d865f1e4f98a818e8ade
Junha Park <tunatone.0111@gmail.com>
2021-04-05 18:48:36 +0000

    search

--- lawgic-web/src/pages/Search.tsx
+++ lawgic-web/src/pages/Search.tsx
@@ -4,25 +4,28 @@ import {
 	Container,
 	FormControlLabel,
 	Grid,
-	Slider,
-	Typography,
+	LinearProgress,
 	makeStyles,
+	Slider,
 	TablePagination,
+	Typography,
 } from "@material-ui/core";
-import React, {
-	useCallback,
-	useState,
-	useEffect,
-	ChangeEventHandler,
-} from "react";
-import { useLocation } from "react-router";
+import _ from "lodash";
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router";
 import useSwr from "swr";
 import Navbar from "../components/Navbar";
 import Prec from "../components/Prec";
-import fetcher, { mockFetcher } from "../services/fetcher";
-import _ from "lodash";
+import fetcher from "../services/fetcher";
 
 const useStyles = makeStyles((theme) => ({
+	loading: {
+		position: "absolute",
+		top: 0,
+	},
+	textCentering: {
+		textAlign: "center",
+	},
 	slider: {
 		marginTop: theme.spacing(5),
 	},
@@ -30,10 +33,11 @@ const useStyles = makeStyles((theme) => ({
 
 function Search() {
 	const classes = useStyles();
+	const history = useHistory();
 	const yearLimit = [1970, new Date().getFullYear()];
 	const query = new URLSearchParams(useLocation().search);
 	const { data, error } = useSwr(
-		`http://localhost:5000/api/search?query=${query.get("query")}`,
+		`${process.env.REACT_APP_API_URL}/api/search?query=${query.get("query")}`,
 		async (url) => {
 			const res = await fetcher(url);
 			return {
@@ -46,6 +50,7 @@ function Search() {
 	const [page, setPage] = useState(0);
 	const [rowsPerPage, setRowsPerPage] = useState(10);
 	const [yearRange, setYearRange] = useState([2000, 2020]);
+	const [onlySupreme, setOnlySupreme] = useState(false);
 	const [filteredPrecs, setFilteredPrecs] = useState<PrecPreview[]>([]);
 	const [pagedPrecs, setPagedPrecs] = useState<PrecPreview[]>([]);
 
@@ -71,11 +76,15 @@ function Search() {
 		if (data) {
 			const { precs } = data;
 			setFilteredPrecs([
-				...precs.filter((p: any) => {
-					let [s, e] = yearRange;
-					if (s === yearLimit[0]) s = 0;
-					return _.inRange(p.date.getFullYear(), s, e + 1);
-				}),
+				...precs
+					.filter((p: PrecPreview) =>
+						onlySupreme ? p.court.code === 400201 : true
+					)
+					.filter((p: PrecPreview) => {
+						let [s, e] = yearRange;
+						if (s === yearLimit[0]) s = 0;
+						return _.inRange(p.date.getFullYear(), s, e + 1);
+					}),
 			]);
 			setPage(0);
 		}
@@ -90,54 +99,70 @@ function Search() {
 	useEffect(filterPrecs, [data]);
 	useEffect(paginatePrecs, [filteredPrecs, rowsPerPage, page]);
 
-	if (error) return <div>failed to load</div>;
-	if (!data) return <div>loading...</div>;
+	if (error) history.push("/error");
 
 	return (
 		<>
-			<Navbar />
-			<Container component="section">
-				<Grid container spacing={2}>
-					<Grid item xs={2}>
-						<Grid container direction="column">
-							<Grid item>
-								<Slider
-									value={yearRange}
-									onChange={handleChangeYearRange}
-									valueLabelDisplay="on"
-									min={yearLimit[0]}
-									max={yearLimit[1]}
-									className={classes.slider}
-								/>
-							</Grid>
-							<Grid item>
-								<FormControlLabel
-									control={<Checkbox color="primary" />}
-									label="대법원판례만 보기"
-								/>
-							</Grid>
-							<Grid item>
-								<Button variant="contained" onClick={filterPrecs}>
-									Apply
-								</Button>
+			{!data ? (
+				<LinearProgress className={classes.loading} />
+			) : (
+				<Container component="section">
+					<Grid container spacing={2}>
+						<Grid item xs={2}>
+							<Grid container direction="column">
+								<Grid item>
+									<Slider
+										value={yearRange}
+										onChange={handleChangeYearRange}
+										valueLabelDisplay="on"
+										min={yearLimit[0]}
+										max={yearLimit[1]}
+										className={classes.slider}
+									/>
+								</Grid>
+								<Grid item>
+									<FormControlLabel
+										control={
+											<Checkbox
+												color="primary"
+												value={onlySupreme}
+												onChange={(e) => setOnlySupreme(e.target.checked)}
+											/>
+										}
+										label="대법원판례만 보기"
+									/>
+								</Grid>
+								<Grid item>
+									<Button variant="contained" onClick={filterPrecs}>
+										Apply
+									</Button>
+								</Grid>
 							</Grid>
 						</Grid>
+						<Grid item xs={10}>
+							<TablePagination
+								component="div"
+								count={filteredPrecs.length}
+								page={page}
+								onChangePage={handleChangePage}
+								rowsPerPage={rowsPerPage}
+								onChangeRowsPerPage={handleChangeRowsPerPage}
+							/>
+							{pagedPrecs.length === 0 ? (
+								<Typography
+									variant="h5"
+									component="span"
+									className={classes.textCentering}
+								>
+									검색된 판례가 없습니다.
+								</Typography>
+							) : (
+								pagedPrecs.map((prec, idx) => <Prec key={idx} prec={prec} />)
+							)}
+						</Grid>
 					</Grid>
-					<Grid item xs={10}>
-						<TablePagination
-							component="div"
-							count={filteredPrecs.length}
-							page={page}
-							onChangePage={handleChangePage}
-							rowsPerPage={rowsPerPage}
-							onChangeRowsPerPage={handleChangeRowsPerPage}
-						/>
-						{pagedPrecs.map((prec, idx) => (
-							<Prec key={idx} prec={prec} />
-						))}
-					</Grid>
-				</Grid>
-			</Container>
+				</Container>
+			)}
 		</>
 	);
 }
